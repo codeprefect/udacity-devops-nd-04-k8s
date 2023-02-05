388e8371e53c1715bc1332ca661db0e92d5312c6
Mubarak Imam <mubarakadeimam@gmail.com>
2023-02-05 09:13:37 +0000

    updated readme.md

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# build and tag docker image
 docker build . --tag codeprefect/udacity-ml:latest
 
+# list all images, lookout for the newly created image
 docker images
 
+# run a docker container based on the new image
 docker run --name udacity-ml -p 8000:80 codeprefect/udacity-ml:latest
--- upload_docker.sh
+++ upload_docker.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 dockerpath=codeprefect/udacity-ml
 
+# you may uncomment the docker build command below if the image does not already exist
+# build and tag docker image
+# docker build . --tag $dockerpath
+
 # Step 2:  
 docker login
 echo "Docker ID and Image: $dockerpath"
